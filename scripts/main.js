fa04c7f4513ec08d7d36ac75ab6abaf1826a3b10
Xuan1030 <61073253+Xuan1030@users.noreply.github.com>
2021-01-05 15:25:15 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/car.jpg') {
-      myImage.setAttribute('src', 'images/car1.jpg');
+      myImage.setAttribute('src', 'images/car1.JPG');
     } else {
       myImage.setAttribute('src', 'images/car.jpg');
     }
